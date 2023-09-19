848369a08f045b24c8bd7b5d3ae904d40764e8ca
Truong Le <truong.le@reapra.sg>
2023-09-20 02:34:49 +0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :recoverable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+end
